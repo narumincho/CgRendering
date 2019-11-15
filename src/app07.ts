a2318b1572682bef4bb896dbde9feadacd5e68e7
narumincho <narumincho.starfy@gmail.com>
2019-11-15 14:46:01 +0900

    add app07print.ts

--- src/app07.ts
+++ src/app07.ts
@@ -44,8 +44,8 @@ varying vec3 vPosition;
 
 void main()
 {
-    gl_FragColor = vec4( (vPosition + vec3(1)) / 2.0, 1.0 );
-    
+    gl_FragColor = vec4( floor((vPosition + vec3(1.0)) * 2.0) / 4.0, 1.0 );
+    // gl_FragColor = vec4( (vPosition + vec3(1.0)) / 2.0, 1.0 );    
 }`
         })
     )
---
+++ src/app07print.ts
@@ -0,0 +1,21 @@
+import { Vector3 } from "three";
+
+const I: Vector3 = new Vector3(1 / 2, -Math.sqrt(3) / 2, 0);
+const N: Vector3 = new Vector3(0, 1, 0);
+const IPrime: Vector3 = I.clone().divideScalar(Math.abs(I.clone().dot(N)));
+for (let ni = 0; ni < 3; ni += 0.01) {
+    const n: number = 1 / ni;
+    const kf2: number =
+        1 /
+        (n ** 2 * IPrime.lengthSq() -
+            I.clone()
+                .add(N)
+                .lengthSq());
+    const T: Vector3 = I.clone()
+        .add(N)
+        .multiplyScalar(Math.sqrt(kf2))
+        .sub(N);
+    console.group("ni=", ni);
+    console.log(T);
+    console.groupEnd();
+}
