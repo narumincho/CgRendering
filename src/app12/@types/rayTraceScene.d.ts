ea385e8ae391e572d5857172e54fda12045aeb2b
narumincho <narumincho.starfy@gmail.com>
2019-12-19 16:04:00 +0900

    fix @types/rayTraceScene.d.ts

--- src/app12/@types/rayTraceScene.d.ts
+++ src/app12/@types/rayTraceScene.d.ts
@@ -1,4 +1,4 @@
-export default type Scene = {
+type Scene = {
   pointlight: {
     x: number;
     y: number;
@@ -11,27 +11,27 @@ export default type Scene = {
     b: number;
   };
   ellipses: Array<{
-        x: number,
-        y: number,
-        z: number,
-        a: number,
-        b: number,
-        c: number,
-        material: Material,
-    }>,
+    x: number;
+    y: number;
+    z: number;
+    a: number;
+    b: number;
+    c: number;
+    material: Material;
+  }>;
   triangles: Array<{
-        x0: number,
-        y0: number,
-        z0: number,
-        x1: number,
-        y1: number,
-        z1: number,
-        x2: number,
-        y2: number,
-        z2: number,
-        material: Material,
-    }>,
-}
+    x0: number;
+    y0: number;
+    z0: number;
+    x1: number;
+    y1: number;
+    z1: number;
+    x2: number;
+    y2: number;
+    z2: number;
+    material: Material;
+  }>;
+};
 
 type Material = {
   r: number;
@@ -41,4 +41,6 @@ type Material = {
   id: number;
   is: number;
   n: number;
-}
+};
+
+export default Scene;
